7686709a6b022ede78f4545de052a77fca0fd1fa
mishalp <mishalclever@gmail.com>
2022-11-05 18:06:58 +0530

    card component created

--- src/components/shope/Shope.js
+++ src/components/shope/Shope.js
@@ -1,11 +1,34 @@
 import React from 'react'
+import products from '../../product'
+import './shope.css'
 
-function Shope() {
-  return (
-    <div>
-        Shope
+const CardBuilder = (props)=>{
+  return(
+    <div className="cards">
+      {props.product.map((item)=>{
+        return <a href="#">
+          <div className="card">
+            <img src={item.image} alt="" />
+            <h2>{item.name}</h2>
+            <p>{item.desc}</p>
+            <h6>{item.cost} ₹</h6>
+            <a href="#"><button>Add to Cart</button></a>
+          </div>
+        </a>
+      })}
     </div>
   )
 }
+
+function Shope() {
+  return (
+    <div className='products'>
+      <h1>Prouducts</h1>
+        <CardBuilder product={products} />
+    </div>
+  )
+}
+
+
 
 export default Shope
---
+++ src/product.js
@@ -0,0 +1,31 @@
+import iPhone from './images/products/mobiles/IPhone14.png'
+import pixel7 from './images/products/mobiles/pixel7.jpg'
+import xiomi12 from './images/products/mobiles/xiomi12.jpg'
+
+
+const products = [
+    {
+        id: 0,
+        name: 'I Phone 14',
+        desc: 'Very nice phone',
+        cost: 100000,
+        image: iPhone,
+    },
+    {
+        id: 1,
+        name: 'Google Pixel 7',
+        desc: 'Very nice phone',
+        cost: 90000,
+        image: pixel7,
+    },
+    {
+        id: 2,
+        name: 'Xiomi 12 Pro',
+        desc: 'very nice phone',
+        cost: 30000,
+        image: xiomi12,
+    }
+
+]
+
+export default products
