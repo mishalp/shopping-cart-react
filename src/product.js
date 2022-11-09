781ee51abbea4e11af6a70a200db75004cfc7b31
mishalp <mishalclever@gmail.com>
2022-11-09 13:39:06 +0530

    more products Added

--- src/product.js
+++ src/product.js
@@ -1,6 +1,11 @@
 import iPhone from './images/products/mobiles/IPhone14.png'
 import pixel7 from './images/products/mobiles/pixel7.jpg'
 import xiomi12 from './images/products/mobiles/xiomi12.jpg'
+import samsungs22 from './images/products/mobiles/samsungs22.jpg'
+import opporeno8 from './images/products/mobiles/opporeno8.jpg'
+import iq007 from './images/products/mobiles/iq007.jpg'
+import honorMagic3 from './images/products/mobiles/honorMagic3.png'
+import nothingPhone from './images/products/mobiles/nothingPhone.jpg'
 
 
 const products = [
@@ -27,7 +32,47 @@ const products = [
         price: 67499,
         image: xiomi12,
         piece: 1,
-    }
+    },
+    {
+        id: 3,
+        name: 'Samsung S22',
+        desc: 'very nice phone',
+        price: 62999,
+        image: samsungs22,
+        piece: 1,
+    },
+    {
+        id: 4,
+        name: 'Oppo Reno 8',
+        desc: 'very nice phone',
+        price: 29999,
+        image: opporeno8,
+        piece: 1,
+    },
+    {
+        id: 5,
+        name: 'iQOO7',
+        desc: 'very nice phone',
+        price: 29990,
+        image: iq007,
+        piece: 1,
+    },
+    {
+        id: 6,
+        name: 'Honor Magic3',
+        desc: 'very nice phone',
+        price: 52790,
+        image: honorMagic3,
+        piece: 1,
+    },
+    {
+        id: 7,
+        name: 'Nothing Phonr',
+        desc: 'very nice phone',
+        price: 32999,
+        image: nothingPhone,
+        piece: 1,
+    },
 
 ]
 
